173a31739d95a830a805a1d08f5016ecc104f73a
Darío Chinea Delgado <dario.chinea.external@atos.net>
2023-09-13 12:31:42 +0100

    finish header

--- src/services/fighter.ts
+++ src/services/fighter.ts
@@ -2,6 +2,6 @@ import { type FighterAPI } from "../types/fighter-api";
 
 export const getFighters = async () => {
   const res = await fetch("http://localhost:5000/ufc/v1/fighter");
-  const data = await res.json();
+  const data = (await res.json()) as FighterAPI[];
   return data;
 };
