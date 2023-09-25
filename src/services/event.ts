c470f98fe7fe55cefa0298b6205870ee8ac168be
Darío Chinea Delgado <dario.chinea.external@atos.net>
2023-09-25 14:34:01 +0100

    Work in progress

--- src/services/event.ts
+++ src/services/event.ts
@@ -1,5 +1,5 @@
 import { type Event } from "../types/event-api";
-
+import { type FightAPI } from "../types/fight-api";
 import { apiUrl } from "./apiConfig";
 
 export const getEventById = async ({ id }: { id: number }) => {
@@ -7,3 +7,16 @@ export const getEventById = async ({ id }: { id: number }) => {
   const data = (await res.json()) as Event;
   return data;
 };
+
+export const getAllEvents = async () => {
+  const res = await fetch(`${apiUrl}ufc/v1/event`);
+  const data = (await res.json()) as Event[];
+  return data;
+};
+
+export const getFightByEventId = async ({ id }: { id: number }) => {
+  const res = await fetch(`${apiUrl}ufc/v1/event/fight/${id}`);
+  const data = (await res.json()) as FightApi[];
+  return data;
+};
+
